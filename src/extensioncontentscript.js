987333728bf0d20865de7a0831193859e315a53d
emfmesquita <edson.mesquita.ismart@gmail.com>
2017-10-03 06:17:01 -0300

    content script continue

--- src/extensioncontentscript.js
+++ src/extensioncontentscript.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import $ from "jquery";
 
-
-// chrome.webRequest.onCompleted.addListener((details) => {
-//     debugger;
-//     $(".more-info-monster").find(".ddb-statblock-monster:first").each((idx, el) => {
-//         const id = $(el).closest(".more-info-monster").prev().attr("data-slug");
-//         console.log(id);
-//     });    
-// }, {urls: ["https://www.dndbeyond.com/monsters/*/more-info"]});
+const bhAddMonster = function(id, name, hp){
+    console.log(id);
+    console.log(name);
+    console.log(hp);
+}
 
 chrome.runtime.onMessage.addListener(() => {
-    $(".more-info-monster").find(".ddb-statblock-monster:first").each((idx, el) => {
-        const id = $(el).closest(".more-info-monster").prev().attr("data-slug");
-        console.log(id);
+    $(".more-info-monster:not(.bh-processed").find(".ddb-statblock-monster:first").each((idx, el) => {
+        const moreInfoDiv = $(el).closest(".more-info-monster");
+        moreInfoDiv.addClass("bh-processed");
+        const rowDiv = moreInfoDiv.prev();
+        const id = rowDiv.attr("data-slug");
+        const name = rowDiv.find(".monster-name a").text();
+        const hp = moreInfoDiv.find(".ddb-statblock-item-hit-points .primary").text();
+
+        const moreInfoBody = $(el).closest(".more-info-body");
+
+        const buttonHtml = '<div class="more-info-footer-details-button"><a class="button button-monsters" href="javascript:void(0);">Add</a></div>';
+        moreInfoBody.prepend(buttonHtml).on("click", () => bhAddMonster(id, name, hp));
     });
 });
