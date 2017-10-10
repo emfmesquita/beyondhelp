6e669ff46cee408d2c160dfa0b54fd6cfc28ab95
emfmesquita <edson.mesquita.ismart@gmail.com>
2017-10-10 05:10:24 -0300

    build clean, two monsters per column

--- src/MonsterList.js
+++ src/MonsterList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import Monster from './Monster';
 import MonsterData from './data/MonsterData';
 import './MonsterList.css';
@@ -7,32 +8,46 @@ import ToMonsterPageButton from "./monsterbuttons/ToMonsterPageButton";
 class MonsterList extends Component {
     constructor(props) {
         super(props);
-        this.buildList = this.buildList.bind(this);
+        this.buildColumn = this.buildColumn.bind(this);
+        this.buildRows = this.buildRows.bind(this);
     }
 
-    buildList() {
-        return this.props.monsters.map((monster: MonsterData, index: number) => {
-            const last = (this.props.monsters.length - 1) === index;
+    buildColumn(monster: MonsterData) {
+        if (!monster) return false;
+        return (
+            <Col className="Monster-list-column" xs={6} key={monster.storageId}>
+                <Monster monster={monster} onRemoveMonster={this.props.onRemoveMonster} />
+            </Col>
+        );
+    }
+
+    buildRows() {
+        const monsters = this.props.monsters;
+        const rows = monsters.map((monster: MonsterData, index: number) => {
+            const even = index % 2 === 0;
+            if (!even) return false;
             return (
-                <li className={last && "Monster-list-last"} key={monster.storageId}>
-                    <Monster monster={monster} onRemoveMonster={this.props.onRemoveMonster} />
-                </li>
+                <Row>
+                    {this.buildColumn(monsters[index])}
+                    {this.buildColumn(index < monsters.length - 1 ? monsters[index + 1] : null)}
+                </Row>
             );
         });
+        return rows;
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <div style={{display: "inline-block"}}>
+                    <div style={{ display: "inline-block" }}>
                         <ToMonsterPageButton monsterId={this.props.id} />
                     </div>
-                    <div style={{display: "inline-block"}} className="Monster-list-header">
+                    <div style={{ display: "inline-block" }} className="Monster-list-header">
                         <p>{this.props.name} (x{this.props.monsters.length}):</p>
                     </div>
                 </div>
-                <ul>{this.buildList()}</ul>
+                <Grid>{this.buildRows()}</Grid>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,78 @@
 const path = require('path');
+const fs = require('fs-extra');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const buildDir = path.resolve(__dirname, 'build');
+const publicDir = path.resolve(__dirname, 'public');
+const popupHtml = path.resolve(__dirname, 'public/extensionpopup.html');
+
+fs.emptyDirSync(buildDir);
+fs.copySync(publicDir, buildDir, {
+    dereference: true,
+    filter: file => file !== popupHtml
+});
+
+const static = {
+    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+    loader: 'url-loader',
+    options: {
+        limit: 10000,
+        name: 'static/media/[name].[hash:8].[ext]',
+    },
+}
+
+const js = {
+    test: /\.(js|jsx)$/,
+    loader: 'babel-loader',
+    options: {
+        presets: ['env', 'flow', 'react'],
+        cacheDirectory: true,
+    }
+}
+
+const css = {
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        { loader: 'css-loader', options: { importLoaders: 1 } }
+    ]
+}
+
+const file = {
+    exclude: [/\.js$/, /\.html$/, /\.json$/],
+    loader: 'file-loader',
+    options: {
+        name: 'static/media/[name].[hash:8].[ext]',
+    }
+}
+
+module.exports = [{
     entry: './src/contentscript/extensioncontentscript.js',
     output: {
         filename: 'extensioncontentscript.js',
-        path: path.resolve(__dirname, 'build')
+        path: buildDir
     },
+    devtool: "cheap-module-eval-source-map",
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env', 'react']
-                }
-            }
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            oneOf: [static, js, css, file]
         }]
     }
-};
+}, {
+    entry: './src/extensionpopup.js',
+    output: {
+        filename: 'extensionpopup.js',
+        path: buildDir
+    },
+    devtool: "cheap-module-eval-source-map",
+    module: {
+        rules: [{
+            oneOf: [static, js, css, file]
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        inject: true,
+        template: popupHtml,
+        filename: "extensionpopup.html"
+    })]
+}];
