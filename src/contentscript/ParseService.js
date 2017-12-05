87bcec25d13a86c796139f2581609c9ff0b52fdd
emfmesquita <edson.mesquita.ismart@gmail.com>
2017-12-04 22:15:43 -0200

    changes for new monster layout and remove of better print layout

--- src/contentscript/ParseService.js
+++ src/contentscript/ParseService.js
@@ -16,7 +16,7 @@ const isOnMonsterDetail = function (path: string) {
 
 const parseList = function (isHomebrew): ParseData[] {
     const parseDataArray = [];
-    $(".more-info-monster:not(.bh-processed)").find(".ddb-statblock-monster:first").each((idx, el) => {
+    $(".more-info-monster:not(.bh-processed)").find(".mon-stat-block").each((idx, el) => {
         // gather monster info from page
         const moreInfoDiv = $(el).closest(".more-info-monster");
         moreInfoDiv.addClass("bh-processed");
@@ -25,8 +25,11 @@ const parseList = function (isHomebrew): ParseData[] {
         const id = rowDiv.attr("data-slug");
         const nameSelector = isHomebrew ? ".list-row-col-name .list-row-name-primary-text" : ".monster-name a";
         const name = rowDiv.find(nameSelector).text().trim();
-        const diceHp = moreInfoDiv.find(".ddb-statblock-item-hit-points .secondary").text().trim();
-        const hp = moreInfoDiv.find(".ddb-statblock-item-hit-points .primary").text().trim();
+
+        const jqHpAttribute = moreInfoDiv.find(".mon-stat-block__attributes .mon-stat-block__attribute:nth-child(2)");
+        const diceHp = jqHpAttribute.find(".mon-stat-block__attribute-data-extra").text().trim();
+        const hp = jqHpAttribute.find(".mon-stat-block__attribute-data-value").text().trim();
+
         const monsterData = new MonsterContentData(id, name, hp, diceHp);
 
         // target
@@ -40,13 +43,14 @@ const parseList = function (isHomebrew): ParseData[] {
 const parseDetail = function (): ParseData[] {
     const path = window.location.pathname;
     const id = path.substring(path.lastIndexOf("/") + 1);
-    const name = $(".monster-name").text().trim();
+    const name = $(".mon-stat-block__name-link").text().trim();
 
-    const infoDiv = $(".monster-info");
-    const diceHp = infoDiv.find(".hp .secondary").text().trim();
-    const hp = infoDiv.find(".hp .primary").text().trim();
+    const jqHpAttribute = $(".mon-stat-block__attributes .mon-stat-block__attribute:nth-child(2)");
 
-    return [new ParseData("prepend", infoDiv, new MonsterContentData(id, name, hp, diceHp))];
+    const diceHp = jqHpAttribute.find(".mon-stat-block__attribute-data-extra").text().trim();
+    const hp = jqHpAttribute.find(".mon-stat-block__attribute-data-value").text().trim();
+
+    return [new ParseData("before", $(".mon-stat-block"), new MonsterContentData(id, name, hp, diceHp))];
 };
 
 class ParseService {
