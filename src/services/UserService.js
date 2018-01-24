c7e0633d82f667ba15760f4f9c250f5295bb01e1
emfmesquita <edson.mesquita.ismart@gmail.com>
2018-01-24 16:07:39 -0200

    user service fix

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -8,10 +8,16 @@ class UserService {
         return new Promise((resolve, reject) => {
             chrome.cookies.getAll({
                 domain: ".dndbeyond.com",
-                name: "User.Username"
+                name: "User"
             }, (cookies) => {
                 const cookie = cookies && cookies[0] ? cookies[0] : null;
-                const username = cookie ? cookie.value : null;
+                const value = cookie ? cookie.value : null;
+                let username = null;
+                if (value) {
+                    value.split("&").forEach(token => {
+                        if (token.startsWith("UserName=")) username = token.replace("UserName=", "");
+                    });
+                }
                 chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve(username);
             });
         });
